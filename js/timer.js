60059ef3098328d000e032fc18216b5a9fa7c238
itagaki <itagakishintaro@jp.fujitsu.com>
2014-08-22 11:58:16 +0900

    first commit

---
+++ js/timer.js
@@ -0,0 +1,73 @@
+'use strict';
+
+var timer, min, sec;
+
+$(function() {
+    $('#start').click(function() {
+        $('#message').text('');
+        if (set()) {
+            timer = setInterval('counter()', 100);
+        }
+    });
+
+    $('#reset').click(function(){
+        min = 0;
+        sec = 0;
+        setView();
+        clearInterval(timer);
+        document.getElementById('sound').pause();
+        $('#message').text('');
+        $('#minutes-setting').val('0');
+        $('#seconds-setting').val('0');
+    });
+});
+
+function set() {
+    // gard
+    if (!Number($('#minutes-setting').val()) && !Number($('#seconds-setting').val())) {
+        $('#message').text('Please set time!');
+        return false;
+    }
+    // minutes
+    if (Number($('#minutes-setting').val())) {
+        min = Number($('#minutes-setting').val());
+    } else {
+        min = 0;
+    }
+    // seconds
+    if (Number($('#seconds-setting').val())) {
+        sec = Number($('#seconds-setting').val());
+    } else {
+        sec = 0;
+    }
+
+    setView();
+    return true;
+}
+
+function setView(){
+    $('#minutes').text(convertNum(min, 2));
+    $('#seconds').text(convertNum(sec, 2));
+}
+
+function convertNum(num, figures) {
+    var str = String(num);
+    while (str.length < figures) {
+        str = "0"+str;
+    }
+    return str;
+}
+
+function counter() {
+    if(min === 0 && sec === 0){
+        clearInterval(timer);
+        document.getElementById('sound').play();
+        $('#message').text('終了でーす。');
+    }else if(sec === 0){
+        sec = 59;
+        min--;
+    }else{
+        sec--;
+    }
+    setView();
+}
